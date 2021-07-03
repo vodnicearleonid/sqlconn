7ba8995baab01a18008bb1d1612d0326dff12541
vodnicearleonid <vodnicear@gmail.com>
2021-07-04 02:33:41 +0300

    oop test less1

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<<!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>OOP Base</title>
 </head>
 <body>
 
@@ -13,13 +13,20 @@
 
 class User {
     public $name;
-    protected $surmane;
-    protected $email;
-    protected $pass;
+    private $surname = "None";
+    private $email;
+    private $login;
+    private $password;
+
 }
+
+$admin = new User();
+$admin->name = "John";
+echo $admin->name;
 
 ?>
 
 </body>
 </html>
+
 
